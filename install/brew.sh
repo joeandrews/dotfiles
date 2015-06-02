656e2dcb2ccc6b09c3af1f4d6148f97e151b820a
Greg B <gblock0@gmail.com>
2015-06-02 11:14:18 -0500

    Updated gitconfig, brew.sh, and deleted nginx.sh

--- install/brew.sh
+++ install/brew.sh
@@ -6,13 +6,8 @@ brew install ack
 brew install tree
 brew install wget
 
-# development server setup
-brew install nginx
-brew install dnsmasq
-
 # development tools
 brew install git
-brew install hub
 brew install macvim --override-system-vim
 brew install reattach-to-user-namespace
 brew install tmux
@@ -20,7 +15,3 @@ brew install zsh
 brew install highlight
 brew install nvm
 brew install z
-
-# gitsh
-brew tap thoughtbot/formulae
-brew install gitsh
